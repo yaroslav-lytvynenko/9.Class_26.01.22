86f39673fa7adf155aff0030fabd7f57e39c57cf
jarisleif <yaroslav.lytvynenko@yahoo.com>
2022-01-31 16:21:37 +0200

    Initial commit

---
+++ src/com/company/Box.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Box {
+    private int height;
+    private int width;
+    private int depth;
+    private String name;
+    private String material;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public Box(int height, int width, int depth) {
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+    }
+
+    public Box(int height, int width, int depth, String name, String material) {
+        this(height, width, depth);
+        this.name = name;
+        this.material = material;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public int setHeight(int height) {
+        this.height = validation(height);
+        return this.height;
+    }
+
+    public int setWidth(int width) {
+        this.width = validation(width);
+        return this.width;
+    }
+
+    public int setDepth(int depth) {
+        this.depth = validation(depth);
+        return this.depth;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int validation(int value) {
+        int i = 0;
+        boolean passValue = false;
+        do {
+            if (i > 0) {
+                System.out.print("Input value: ");
+                value = scanner.nextInt();
+            }
+            if (value <= 0) {
+                System.out.println("Error! Incorrect value\n");
+            } else {
+                passValue = true;
+            }
+            i++;
+        } while (!passValue);
+        return value;
+    }
+
+    public int volume() {
+        return height * width * depth;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Box one = new Box(3, 4, 3);
+        Box two = new Box(7, 5, 2);
+        Box three = new Box(2, 6, 8);
+        Box four = new Box(10, 3, 2);
+        Box five = new Box(6, 2, 6);
+
+        one.setName("First");
+        two.setName("Second");
+        three.setName("Third");
+        four.setName("Fourth");
+        five.setName("Fifth");
+
+        one.setMaterial("Wood");
+        two.setMaterial("Plastic");
+        three.setMaterial("Metal");
+        four.setMaterial("Glass");
+        five.setMaterial("Carton");
+
+        one.setHeight(0);
+
+        System.out.println("vol of " + one.getName() + ":  " + one.volume()
+                        + "\nvol of " + two.getName() + ": " + two.volume()
+                        + "\nvol of " + three.getName() + ":  " + three.volume()
+                        + "\nvol of " + four.getName() + ": " + four.volume()
+                        + "\nvol of " + five.getName() + ":  " + five.volume());
+    }
+
+}

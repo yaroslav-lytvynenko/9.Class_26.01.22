c92a7a5ef7d26752d22c45ce8ac37363fb0d9f6a
jarisleif <yaroslav.lytvynenko@yahoo.com>
2022-02-10 19:45:08 +0200

    10.02.22

---
+++ src/com/company/Warehouse.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Warehouse {
+    private int capacity;
+    private String name;
+    private int placesTaken = 0;
+    private Box[] warehouseBoxes;
+
+    public Warehouse(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.warehouseBoxes = new Box[this.capacity];
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String setName(String name){
+        this.name = name;
+        return this.name;
+    }
+
+    public void addBox(Box box) {
+        int counter = placesTaken + 1;
+        if (counter < capacity) {
+            warehouseBoxes[placesTaken] = box;
+            placesTaken++;
+        } else if (counter == capacity) {
+            warehouseBoxes[placesTaken] = box;
+            placesTaken++;
+            System.out.println("\nThe warehouse " + getName() + " is full now, last box is: " + box.getName() + "\n");
+        } else {
+            System.out.println("Impossible to add " + box.getName() + ", no place left in " + getName());
+        }
+    }
+
+    @Override
+    public String toString(){
+        System.out.println("\n" + getName() + " contents:");
+        for(int i = 0; i < warehouseBoxes.length; i++){
+            String a = warehouseBoxes[i].toString();
+            System.out.println(a);
+        }
+        return "";
+    }
+}
